f97f4f4588cdf9627cb301ce8b987384ce181234
Helen Poe <helenpoe@MacBook-Air-Helen.local>
2022-04-16 16:14:53 +0200

    initial commit

---
+++ IOSTraineeApp/IOSTraineeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTraineeAppApp.swift
+//  IOSTraineeApp
+//
+//  Created by Helen Poe on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct IOSTraineeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
